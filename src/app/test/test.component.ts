72aba710dda4c99f7f1c695b08a0a30bb62c628d
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-24 14:42:16 +0100

    binded some data with string interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -27,6 +27,15 @@ export class TestComponent
   constructor() {
     console.log('constructor');
   }
+  dogs = [
+    {
+      name: 'Alpina',
+      breed: 'Golden-Retriever',
+      description: `The Shiba Inu is the smallest of the six original and distinct spitz
+      breeds of dog from Japan. A small, agile dog that copes very well with
+      mountainous terrain, the Shiba Inu was originally bred for hunting.`,
+    },
+  ];
   ngOnInit(): void {
     console.log('ngOnInit');
   }
