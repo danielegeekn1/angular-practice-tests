00c1625abff26025a5ecd984fe2a8e3290193504
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-24 14:02:12 +0100

    implemented all lifecycle component in our test.component.ts file

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
 })
-export class TestComponent implements OnInit {
+export class TestComponent
+  implements
+    OnInit,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit,
+    DoCheck,
+    OnDestroy
+{
   constructor() {
     console.log('constructor');
   }
   ngOnInit(): void {
     console.log('ngOnInit');
+  }
+  ngDoCheck(): void {
+    console.log('doCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 }
