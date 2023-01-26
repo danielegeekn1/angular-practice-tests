b4c9416418d7a1884fb9fccb3ae115337aa5e8b9
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-26 13:53:13 +0100

    passed some data through our appHiglight directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import {
 export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('greetingsInput') inputValue!: ElementRef<HTMLInputElement>;
   title = 'Angular-from-scratch';
+  color = 'purple';
   singers = [
     {
       name: 'Jon Bon Jovi',
--- src/app/directives/highlight/highlight.directive.ts
+++ src/app/directives/highlight/highlight.directive.ts
@@ -1,12 +1,14 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective {
+  @Input() appHighlight = '';
   constructor(private element: ElementRef) {}
+
   @HostListener('mouseenter') onMouseEnter() {
-    this.changeColor('green');
+    this.changeColor(this.appHighlight);
   }
   @HostListener('mouseleave') onMouseLeave() {
     this.changeColor('red');
