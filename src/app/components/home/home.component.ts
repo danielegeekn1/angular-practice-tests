145742033445b68abdb5b0f29005d67b26b1abae
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-30 19:26:03 +0100

    added functionality to our form with ngForm associated to a template variable, and ngModel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  constructor() {}
+  onSubmit(form: NgForm) {
+    console.log('form', form);
+  }
 }
