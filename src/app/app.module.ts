0fa7369dd5cc6d0eb02f7649e5dba7317089958f
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-31 12:29:02 +0100

    added our first reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
@@ -37,6 +37,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatCardModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  homeform: FormGroup;
   constructor() {}
+  ngOnInit(): void {
+    this.homeform = new FormGroup({
+      name: new FormControl('Daniele'),
+      surname: new FormControl(),
+      color: new FormControl(),
+    });
+  }
   onSubmit(form: NgForm) {
     console.log('form', form);
+  }
+  onSubmitR() {
+    console.log(this.homeform);
   }
 }
